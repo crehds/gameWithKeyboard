8dac5bc3f2e01792418d68c41cab9796c8eb5bfa
Carlos Rodriguez <carlos15erh@gmail.com>
2022-12-20 22:59:34 -0500

    Update main logic with new rules
    eslint, wsl

--- src/components/functions/index.js
+++ src/components/functions/index.js
@@ -1,9 +1,49 @@
 import Swal from 'sweetalert2';
 
-export function setupGame(levels, setConfig) {
-  let teclas = generarTeclas(levels);
-  const result = siguienteNivel(0, levels, teclas, setConfig);
-  return result;
+// genera una tecla aleatoria para pulsar
+function generarTeclaAleatoria() {
+  const min = 65;
+  const max = 90;
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function generarTeclas(niveles) {
+  return new Array(niveles).fill(0).map(generarTeclaAleatoria);
+}
+
+// obtiene las propiedades de la tecla presionada
+function getElementByKeyCode(keyCode) {
+  return document.querySelector(`[data-key="${keyCode}"]`);
+}
+
+// tuve que refactorizar el código por la structura de styled-components
+function deactivate(el) {
+  if (el.classList.contains('success')) {
+    el.classList.remove('success');
+  }
+  if (el.classList.contains('fail')) {
+    el.classList.remove('fail');
+  }
+  if (el.classList.contains('active')) {
+    el.classList.remove('active');
+  }
+}
+
+// activara la tecla segun si se presiono o no
+function activate(keyCode, opts = {}) {
+  const el = getElementByKeyCode(keyCode); // se le asigna a la variable el, la tecla presionada
+  if (!el) return null;
+  el.classList.add('active'); // se le agrega la clase de css active
+  // se invoca al atributo del objeto opts, es una atributo predeterminado
+  // asi como el codigo que tiene cada tecla
+  if (opts.success) {
+    el.classList.add('success'); // esto no es el atributo, es el nombre de la clase a añadir(css)
+  } else if (opts.fail) {
+    el.classList.add('fail'); // esto no es el atributo, es el nombre de la clase a añadir(css)
+  }
+
+  // servira para eliminar el color, sea que se presiono o no la tecla correcta
+  return setTimeout(() => deactivate(el), 500);
 }
 
 export function siguienteNivel(nivelActual, levels, teclas, setConfig) {
@@ -22,17 +62,7 @@ export function siguienteNivel(nivelActual, levels, teclas, setConfig) {
     showConfirmButton: false,
   });
 
-  for (let i = 0; i <= nivelActual; i++) {
-    setTimeout((teclas) => activate(teclas[i]), 1000 * (i + 1) + 1000, teclas);
-    if (i === nivelActual) {
-      setTimeout(
-        () => window.addEventListener('keydown', onkeydown),
-        1000 * (i + 1) + 1400
-      );
-    }
-  }
-
-  let i = 0; //para analizar cada tecla desde el principio
+  let i = 0; // para analizar cada tecla desde el principio
   let teclaActual = teclas[i];
 
   function onkeydown(ev) {
@@ -46,12 +76,12 @@ export function siguienteNivel(nivelActual, levels, teclas, setConfig) {
       return null;
     }
     if (ev.keyCode === teclaActual) {
-      //compara la tecla presionada con la que se mostro a pulsar
+      // compara la tecla presionada con la que se mostro a pulsar
       activate(teclaActual, { success: true });
-      i++;
+      i += 1;
 
-      //solo entrara aqui si se pulso correctamente y la misma cantidad de veces
-      //que el numero del nivel. nivel 1 una tecla a pulsar y asi...
+      // solo entrara aqui si se pulso correctamente y la misma cantidad de veces
+      // que el numero del nivel. nivel 1 una tecla a pulsar y asi...
       if (i > nivelActual) {
         //
         window.removeEventListener('keydown', onkeydown);
@@ -59,74 +89,48 @@ export function siguienteNivel(nivelActual, levels, teclas, setConfig) {
       }
 
       teclaActual = teclas[i];
-    } else {
-      activate(ev.keyCode, { fail: true });
-      window.removeEventListener('keydown', onkeydown);
-      setTimeout(function () {
-        Swal.fire({
-          title: 'PARA ESO?, entrena la memoria',
-          text: 'Otra oportunidad?',
-          icon: 'error',
-          showCancelButton: true,
-          confirmButtonText: 'Sí',
-          cancelButtonText: 'No',
-        }).then((ok) => {
-          if (ok.value) {
-            //cuanto sufri para esto....
-            teclas = generarTeclas(levels);
-            siguienteNivel(0, levels, teclas, setConfig);
-          } else {
-            return setConfig({ levels: 0, playing: false });
-          }
-        });
-      }, 400);
-    } //termina else
-  } //termina funcion onkeydown
-} //termina funcion siguiente nivel*/
+      return true;
+    }
 
-function generarTeclas(niveles) {
-  return new Array(niveles).fill(0).map(generarTeclaAleatoria);
-}
+    activate(ev.keyCode, { fail: true });
+    window.removeEventListener('keydown', onkeydown);
+    return setTimeout(() => {
+      Swal.fire({
+        title: 'PARA ESO?, entrena la memoria',
+        text: 'Otra oportunidad?',
+        icon: 'error',
+        showCancelButton: true,
+        confirmButtonText: 'Sí',
+        cancelButtonText: 'No',
+      }).then((ok) => {
+        if (ok.value) {
+          // cuanto sufri para esto....
+          let tmp = teclas.slice();
+          tmp = generarTeclas(levels);
+          return siguienteNivel(0, levels, tmp, setConfig);
+        }
 
-//genera una tecla aleatoria para pulsar
-function generarTeclaAleatoria() {
-  const min = 65;
-  const max = 90;
-  return Math.round(Math.random() * (max - min) + min);
-}
+        return setConfig({ levels: 0, playing: false });
+      });
+    }, 400);
+    // termina else
+  } // termina funcion onkeydown
 
-//obtiene las propiedades de la tecla presionada
-function getElementByKeyCode(keyCode) {
-  return document.querySelector(`[data-key="${keyCode}"]`);
-}
-
-//activara la tecla segun si se presiono o no
-function activate(keyCode, opts = {}) {
-  console.log('en el activate');
-  const el = getElementByKeyCode(keyCode); //se le asigna a la variable el, la tecla presionada
-  if (!el) return null;
-  el.classList.add('active'); //se le agrega la clase de css active
-  //se invoca al atributo del objeto opts, es una atributo predeterminado
-  //asi como el codigo que tiene cada tecla
-  if (opts.success) {
-    el.classList.add('success'); //esto no es el atributo, es el nombre de la clase a añadir(css)
-  } else if (opts.fail) {
-    el.classList.add('fail'); //esto no es el atributo, es el nombre de la clase a añadir(css)
+  for (let j = 0; j <= nivelActual; j += 1) {
+    setTimeout((keyboard) => activate(keyboard[j]), 1000 * (j + 1) + 1000, teclas);
+    if (j === nivelActual) {
+      return setTimeout(
+        () => window.addEventListener('keydown', onkeydown),
+        1000 * (j + 1) + 1400,
+      );
+    }
   }
 
-  //servira para eliminar el color, sea que se presiono o no la tecla correcta
-  setTimeout(() => deactivate(el), 500);
-}
+  return false;
+} // termina funcion siguiente nivel*/
 
-//tuve que refactorizar el código por la structura de styled-components
-function deactivate(el) {
-  if (el.classList.contains('success')) {
-    el.classList.remove('success');
-  }
-  if (el.classList.contains('fail')) {
-    el.classList.remove('fail');
-  }
-  if (el.classList.contains('active')) {
-    el.classList.remove('active');
-  }
+export function setupGame(levels, setConfig) {
+  const teclas = generarTeclas(levels);
+  const result = siguienteNivel(0, levels, teclas, setConfig);
+  return result;
 }
