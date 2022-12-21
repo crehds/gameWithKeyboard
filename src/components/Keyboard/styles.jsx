1719c66a523e9059bc087ab20853064c5ddab466
Carlos Rodriguez <carlos15erh@gmail.com>
2022-12-20 22:56:47 -0500

    Update keyboard with new rules
    eslint, wsl, fix filename styles

---
+++ src/components/Keyboard/styles.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const KeyboardWrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export default KeyboardWrapper;
