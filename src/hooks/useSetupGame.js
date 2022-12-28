7e3c87e84c5a37fb71fde7166abea67fc3a99954
Carlos Rodriguez <carlos15erh@gmail.com>
2022-12-28 06:26:09 -0500

    Refactor game logic with hook

--- src/hooks/useSetupGame.js
+++ src/hooks/useSetupGame.js
@@ -1,24 +1,49 @@
 import { useEffect, useState } from 'react';
+import { generarTeclas } from '../gameSetup/utils';
 
-import initGame from '../gameSetup/init';
 import configModal from '../utils/configModal';
+import nextLevel from './nextLevel';
 
 const INITIAL_CONFIG = {
   levels: 0,
   playing: false,
+  currentLevel: 0,
+  boardKeys: [],
 };
 
 function useSetupGame() {
-  const [config, setConfig] = useState(INITIAL_CONFIG);
+  const [config, setConfig] = useState({ ...INITIAL_CONFIG });
+
+  const handleLevel = (level, reset) => {
+    if (reset) {
+      const boardKeys = generarTeclas(config.levels);
+      return setConfig({ ...config, currentLevel: 0, boardKeys });
+    }
+
+    if (level) {
+      return setConfig({ ...config, currentLevel: level });
+    }
+
+    return setConfig({ ...INITIAL_CONFIG });
+  };
 
   const handleConfig = async () => {
-    const newConfig = await configModal();
-    setConfig(newConfig);
+    const { levels, playing } = await configModal();
+    const boardKeys = generarTeclas(levels);
+    setConfig({
+      ...config, levels, playing, boardKeys,
+    });
+  };
+
+  const handleGame = () => {
+    const { currentLevel, levels, boardKeys } = config;
+    nextLevel(currentLevel, levels, boardKeys, handleLevel);
   };
 
   useEffect(() => {
-    if (config.playing) {
-      initGame(config.levels, setConfig);
+    const { playing } = config;
+    if (playing) {
+      handleGame();
     }
   }, [config]);
 
