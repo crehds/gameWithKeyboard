7c6c84756cf8d0aa120ff1c37999f670c8e3ccba
Carlos Rodriguez <carlos15erh@gmail.com>
2023-01-05 23:27:18 -0500

    Add handleACtivateKeys util

--- src/utils/game.js
+++ src/utils/game.js
@@ -4,7 +4,7 @@ export function validateKeyCode({ keyCode, min, max }) {
   return keyCode > min || keyCode < max;
 }
 
-export function handleResult(keyCode, indexKey, currentLevel, currentKey) {
+export function handleKeyResult(keyCode, indexKey, currentLevel, currentKey) {
   if (validateKeyCode({ keyCode, min: 65, max: 90 })) {
     if (keyCode === currentKey) {
       activate(currentKey, { success: true });
@@ -20,5 +20,17 @@ export function handleResult(keyCode, indexKey, currentLevel, currentKey) {
     return 'lose';
   }
 
-  return null;
+  return 'out';
+}
+
+export function handleActivateKeys({ currentLevel, boardKeys, onKeyDown }) {
+  for (let j = 0; j <= currentLevel; j += 1) {
+    setTimeout((keyboard) => activate(keyboard[j]), 1000 * (j + 1) + 1000, boardKeys);
+    if (j === currentLevel) {
+      setTimeout(
+        () => window.addEventListener('keydown', onKeyDown),
+        1000 * (j + 1) + 1400,
+      );
+    }
+  }
 }
